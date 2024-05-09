4b8c7f4cfd24356b16d70f805898fed9e8c3e226
Valacor <valyrious.informatics@gmail.com>
2024-05-09 15:36:35 +0200

    Create basic solution

---
+++ LearnShop.Api/Controllers/Authentication/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using LearnShop.Api.DataTransfer.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnShop.Api.Controllers.Authentication;
+
+[Route("api/Auth")]
+[ApiController]
+[AllowAnonymous]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginData payload)
+    {
+
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync()
+    {
+
+    }
+}
